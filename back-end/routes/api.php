cef1303c752b80d100da948df7bcf0b04be85aab
wfreitass <wiltterfreitasw@gmal.com>
2024-12-16 21:48:21 -0300

    api user

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -4,13 +4,6 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:api');
-Route::get('/user', function (Request $request) {
-    return 1;
-});
-
 Route::prefix('auth')->group(function (): void {
     Route::post('/register', [AuthController::class, 'createUser']);
     Route::post('/login', [AuthController::class, 'loginUser']);
@@ -19,9 +12,3 @@ Route::prefix('auth')->group(function (): void {
     Route::get('user', [AuthController::class, 'user'])
         ->middleware('auth:api');
 });
-
-// Route::middleware(['auth:sanctum'])->resource('animal', AnimalController::class);
-
-Route::get('/teste', function (Request $request) {
-    return dd('Boolerplate');
-})->middleware('auth:api');

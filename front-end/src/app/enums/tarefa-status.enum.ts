cb2a3028f305610e622cbedc31a7c7769dbc408e
wfreitass <wiltterfreitasw@gmal.com>
2025-01-19 19:41:49 -0300

    first commit

--- front-end/src/app/enums/tarefa-status.enum.ts
+++
@@ -1,4 +0,0 @@
-export enum TarefaStatus {
-  PENDENTE = 'pendente',
-  CONCLUIDA = 'concluido'
-}
--- front-end/src/app/interfaces/ApiResponse.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface ApiResponse<T> {
-    status: number;
-    message: string;
-    data: T;
-    errors: any;
-  }
